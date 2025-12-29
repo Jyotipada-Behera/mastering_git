78073f478773a848f19af3a5505f35ed96e8e28c
Jyotipada <207305228+Jyotipada-Behera@users.noreply.github.com>
2025-12-29 09:38:27 +0530

    Learning numpy

---
+++ main.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+print(np.zeros((3, 4)))
+print(np.ones((2, 5)))
+print(np.full((2, 3), 7))
+print(np.zeros((2,3), dtype=int)+7)
+print(np.eye(4))
+print(np.arange(0, 10, 2))
+print(np.empty((3, 3)))
